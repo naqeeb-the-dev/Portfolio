217096961bfa694dd82e21e8339fa6deb39478d2
Naqeeb Abid <naqeeb.abid1234@gmail.com>
2023-06-17 22:01:02 +0500

    Uploaded files to github

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import * as THREE from 'three';
+import * as THREE from './node_modules/three/build/three.module.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Setup
