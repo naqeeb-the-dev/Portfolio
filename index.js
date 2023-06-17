edd9af7fef6f4cc26b14038e7766e4e5f6711c07
Naqeeb Abid <naqeeb.abid1234@gmail.com>
2023-06-17 22:10:34 +0500

    Updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import * as THREE from 'three';
+import * as THREE from '/build/three.module.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Setup
