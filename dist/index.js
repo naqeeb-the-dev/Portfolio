cbbe5fe4e326927d8148c037840be898b7a9e516
Naqeeb Abid <naqeeb.abid1234@gmail.com>
2023-06-18 11:28:01 +0500

    Uploading

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-import './style.css';
+// import './style.css';
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
 
